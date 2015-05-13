c474c0a2b911ba90f1a7e7bc4e2636d03026e9e1
maron <malongwang@gmail.com>
2015-05-13 21:41:55 +0800

    prettify the comments

--- algorithms/zig_zag_conversion/zig_zag_conversion.c
+++ algorithms/zig_zag_conversion/zig_zag_conversion.c
@@ -1,27 +1,27 @@
 // Source : https://oj.leetcode.com/problems/zigzag-conversion/
-// // Author : Wang MaLong
-// // Date   : 2015-05-13
-// // Runtime: 8 ms
-//
-// /********************************************************************************** 
-// * 
-// * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
-// * (you may want to display this pattern in a fixed font for better legibility)
-// * 
-// * P   A   H   N
-// * A P L S I I G
-// * Y   I   R
-// * 
-// * And then read line by line: "PAHNAPLSIIGYIR"
-// * 
-// * Write the code that will take a string and make this conversion given a number of rows:
-// * 
-// * string convert(string text, int nRows);
-// * 
-// * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
-// * 
-// *               
-// **********************************************************************************/
+// Author : Wang MaLong
+// Date   : 2015-05-13
+// Runtime: 8 ms
+
+/********************************************************************************** 
+* 
+* The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+* (you may want to display this pattern in a fixed font for better legibility)
+* 
+* P   A   H   N
+* A P L S I I G
+* Y   I   R
+* 
+* And then read line by line: "PAHNAPLSIIGYIR"
+* 
+* Write the code that will take a string and make this conversion given a number of rows:
+* 
+* string convert(string text, int nRows);
+* 
+* convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+* 
+*               
+**********************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
