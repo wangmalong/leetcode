a62c113fe96b2fc646f0359fd16eb8faab0d2116
maron <malongwang@gmail.com>
2015-05-15 09:50:37 +0800

    expand a tab to 4 space

--- algorithms/roman_to_integer/roman_to_integer.c
+++ algorithms/roman_to_integer/roman_to_integer.c
@@ -18,50 +18,50 @@ typedef enum{I=1,V=5,X=10,L=50,C=100,D=500,M=1000} roman;
 
 int romanToInt(char *s)
 {
-	int ret = 0, len=0;
-	roman cur, pre;
+    int ret = 0, len=0;
+    roman cur, pre;
 
-	len = strlen(s);
-	cur = s[len-1];
-	pre = I;
-	
+    len = strlen(s);
+    cur = s[len-1];
+    pre = I;
+    
 
-	while(len>0)
-	{
-		switch(s[len-1])
-		{
-			case 'I':
-				cur = I; break;
-			case 'V':
-				cur = V; break;
-			case 'X':
-				cur = X; break;
-			case 'L':
-				cur = L; break;
-			case 'C':
-				cur = C; break;
-			case 'D':
-				cur = D; break;
-			case 'M':
-				cur = M; break;
-		}
+    while(len>0)
+    {
+        switch(s[len-1])
+        {
+            case 'I':
+                cur = I; break;
+            case 'V':
+                cur = V; break;
+            case 'X':
+                cur = X; break;
+            case 'L':
+                cur = L; break;
+            case 'C':
+                cur = C; break;
+            case 'D':
+                cur = D; break;
+            case 'M':
+                cur = M; break;
+        }
 
-		if(cur >= pre) ret+=cur; 
-		else ret -= cur;
-		
-		pre=cur;
-		len--;
-	}
+        if(cur >= pre) ret+=cur; 
+        else ret -= cur;
+        
+        pre=cur;
+        len--;
+    }
 
-	return ret;
+    return ret;
 }
 
 int main()
 {
-	printf("%s = %d\n", "I", romanToInt("I"));
-	printf("%s = %d\n", "II", romanToInt("II"));
-	printf("%s = %d\n", "IV", romanToInt("IV"));
-	printf("%s = %d\n", "VI", romanToInt("VI"));
-	printf("%s = %d\n", "MVI", romanToInt("MVI"));
-	printf("%s = %d\n", "MIV", romanToInt("MIV"));
+    printf("%s = %d\n", "I", romanToInt("I"));
+    printf("%s = %d\n", "II", romanToInt("II"));
+    printf("%s = %d\n", "IV", romanToInt("IV"));
+    printf("%s = %d\n", "VI", romanToInt("VI"));
+    printf("%s = %d\n", "MVI", romanToInt("MVI"));
+    printf("%s = %d\n", "MIV", romanToInt("MIV"));
 }
