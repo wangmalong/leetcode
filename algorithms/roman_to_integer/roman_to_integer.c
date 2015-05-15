961bd641632c50ddd296eaf6a27d949cc86891e8
maron <malongwang@gmail.com>
2015-05-15 09:48:27 +0800

    solve the problem: roman to integer

---
+++ algorithms/roman_to_integer/roman_to_integer.c
@@ -0,0 +1,67 @@
+// Source : https://oj.leetcode.com/problems/roman-to-integer/
+// // Author : Wang MaLong
+// // Date   : 2015-05-15
+// // Runtime: 16ms
+
+/**********************************************************************************
+*
+* Given a roman numeral, convert it to an integer.
+* 
+* Input is guaranteed to be within the range from 1 to 3999.
+***********************************************************************************/
+
+
+#include <stdio.h>
+#include <string.h>
+
+typedef enum{I=1,V=5,X=10,L=50,C=100,D=500,M=1000} roman;
+
+int romanToInt(char *s)
+{
+	int ret = 0, len=0;
+	roman cur, pre;
+
+	len = strlen(s);
+	cur = s[len-1];
+	pre = I;
+	
+
+	while(len>0)
+	{
+		switch(s[len-1])
+		{
+			case 'I':
+				cur = I; break;
+			case 'V':
+				cur = V; break;
+			case 'X':
+				cur = X; break;
+			case 'L':
+				cur = L; break;
+			case 'C':
+				cur = C; break;
+			case 'D':
+				cur = D; break;
+			case 'M':
+				cur = M; break;
+		}
+
+		if(cur >= pre) ret+=cur; 
+		else ret -= cur;
+		
+		pre=cur;
+		len--;
+	}
+
+	return ret;
+}
+
+int main()
+{
+	printf("%s = %d\n", "I", romanToInt("I"));
+	printf("%s = %d\n", "II", romanToInt("II"));
+	printf("%s = %d\n", "IV", romanToInt("IV"));
+	printf("%s = %d\n", "VI", romanToInt("VI"));
+	printf("%s = %d\n", "MVI", romanToInt("MVI"));
+	printf("%s = %d\n", "MIV", romanToInt("MIV"));
+}
